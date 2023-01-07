f3a6cb0a6ba40b94a9819ed146c30f4fa10c8c6d
Carmen Chow <carmendevelops@gmail.com>
2023-01-07 18:59:03 -0500

    add new schema

--- person.js
+++ person.js
@@ -1,6 +1,12 @@
-// Mongoose
-
+// Require Mongoose
 const mongoose = require('mongoose');
 
+// Define a schema
 const schema = mongoose.schema; 
+
+const personSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  age: Number,
+});
 
